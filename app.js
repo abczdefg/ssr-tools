d127576faeafae9877792397203273b4ebd8ec85
abczdefg <304275574@qq.com>
2018-05-30 19:51:00 +0800

    Add ssr.exe

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const SS = require('./ss.js')
 const ProcessController = require('./processController.js')
 // 子进程需要保持打开才能运行exe
 // 改用bat方式执行
 
 const option = {
-  dir: 'E:\\工具\\网络工具\\梯子\\ShadowsocksR',
+  dir: path.resolve(__dirname, 'ShadowsocksR'),
   exeName: 'ShadowsocksR.exe',
   configName: 'gui-config.json',
   url: 'http://ss.ishadowx.com/'
--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ echo 运行app.js，更新SSR账户
 node app.js
 taskkill /im ShadowsocksR.exe /f
 echo 运行 ShadowsocksR.exe
-start "" "E:\工具\网络工具\梯子\ShadowsocksR\ShadowsocksR.exe"
+start "" "ShadowsocksR\ShadowsocksR.exe"
 @exit
