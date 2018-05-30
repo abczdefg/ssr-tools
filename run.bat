123c4e63dd4f2e4cfd5e8337c8fa0873818d47c6
abczdefg <304275574@qq.com>
2018-05-30 19:54:45 +0800

    Fixed path in bat

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ echo 运行app.js，更新SSR账户
 node app.js
 taskkill /im ShadowsocksR.exe /f
 echo 运行 ShadowsocksR.exe
-start "" "ShadowsocksR\ShadowsocksR.exe"
+start "" "%cd%\ShadowsocksR\ShadowsocksR.exe"
 @exit
